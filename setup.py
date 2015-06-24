6431173b9f98f3997bb3bf569ba0f789a1da6d62
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-06-24 20:49:20 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='EoC Battle Mocks',
+    version='0.1',
+    description='Mocks for "Empire of Code" Battle',
+    author='Bryukhanov Valentin',
+    author_email='bryukh@checkio.org',
+)
